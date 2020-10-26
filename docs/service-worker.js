0d56ad90798b72f0c2d45e2f9da8b6931535c4cd
arilybaert <arilybae@arteveldehs.be>
2020-10-26 16:33:20 +0100

    tst

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/x-reality/precache-manifest.d2d138e6a382df1cbebf541d2acfc3ae.js"
+  "/x-reality/precache-manifest.aa791b3908d92921379af73a261bd1f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/x-reality/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
