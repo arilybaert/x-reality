e5b43d271437c87d509494ad922bc56a5234655e
arilybaert <arilybae@arteveldehs.be>
2020-10-26 16:23:06 +0100

    new public folder

---
+++ docs/build/precache-manifest.aa791b3908d92921379af73a261bd1f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214bb359c4ea4e977e453e1f36ab982a",
+    "url": "/x-reality/index.html"
+  },
+  {
+    "revision": "dbf3dfd03f44bfcc141a",
+    "url": "/x-reality/static/css/main.36c22187.chunk.css"
+  },
+  {
+    "revision": "ac8f063ca6f99ce17cf5",
+    "url": "/x-reality/static/js/2.09d5909f.chunk.js"
+  },
+  {
+    "revision": "839ea6260e7558febf83d417311d927b",
+    "url": "/x-reality/static/js/2.09d5909f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf3dfd03f44bfcc141a",
+    "url": "/x-reality/static/js/main.13b626c5.chunk.js"
+  },
+  {
+    "revision": "3d359f0097e749eb6ee5",
+    "url": "/x-reality/static/js/runtime-main.fd09c317.js"
+  },
+  {
+    "revision": "8cf117b7abe71cec11a06bfd1a47130f",
+    "url": "/x-reality/static/media/ari_lybaert_toy.8cf117b7.glb"
+  },
+  {
+    "revision": "ce019e05cd7b57125c2c009f35c43032",
+    "url": "/x-reality/static/media/glTF.ce019e05.glb"
+  },
+  {
+    "revision": "fdd6049581f7de1413dc09ef19660491",
+    "url": "/x-reality/static/media/suzanne.fdd60495.glb"
+  }
+]);
